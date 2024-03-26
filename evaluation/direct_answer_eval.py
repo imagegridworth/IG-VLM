f2e9c8ceeef171180ac0c0c4336348246a3734f3
doublekwsj <imagegrid.video@gmail.com>
2024-03-27 08:52:48 +0900

    .

--- evaluation/direct_answer_eval.py
+++ evaluation/direct_answer_eval.py
@@ -1,3 +1,10 @@
+"""
+ Copyright (c) 2024, Deep Representation Learning Research Group, Seoul National University.
+ All rights reserved.
+ SPDX-License-Identifier: BSD-3-Clause
+ For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+"""
+
 import pandas as pd
 
 
